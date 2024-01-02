19cc79b5eef1bff62056a292321470051af7b8b0
peterfritz369@gmail.com <peterfritz369@gmail.com>
2024-01-02 03:19:58 +0000

    Edited Toggle

--- server_code/Toggle.py
+++ server_code/Toggle.py
@@ -9,22 +9,5 @@ magic_mirror_running = False
 @anvil.server.callable
 def Toggle():
     global magic_mirror_running
-
-    try:
-        if not magic_mirror_running:
-            # Magic Mirror is not running, start it
-            # Uncomment the line below and replace 'your_start_command' with the actual command to start your Magic Mirror
-            # os.system('your_start_command')
-            f'cd ~/MagicMirror && npm start'
-            magic_mirror_running = True
-            return {"status": "Magic Mirror started"}
-        else:
-            # Magic Mirror is running, stop it
-            # Uncomment the line below and replace 'your_stop_command' with the actual command to stop your Magic Mirror
-            # os.system('your_stop_command')
-            magic_mirror_running = False
-            return {"status": "Magic Mirror stopped"}
-
-    except Exception as e:
-        return {"error": str(e)}
+    f'cd ~/MagicMirror && npm start'
 
